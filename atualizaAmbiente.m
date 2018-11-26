883111e1a37141028dd8b695cb6769fd4edbd1dd
MICROTON\andre.lunelli <andre@microton.com.br>
2018-11-26 10:44:53 -0200

    funcao atualiza ambiente com novas opcoes

--- atualizaAmbiente.m
+++ atualizaAmbiente.m
@@ -1,47 +1,84 @@
 % Alunos: André, Karin e Simon
 function [modSala, modX, modY] = atualizaAmbiente(sala, acao, x, y)
 
+global parede;
+
 modSala = sala;
 
 if acao == 5
-    modSala(x, y) = 0; %estado 0 = limpo
-    %retorna as posições x e y
-    modX = x;
-    modY = y;
+  modSala(x, y) = 0; %estado 0 = limpo
+  %retorna as posições x e y
+  modX = x;
+  modY = y;
+  
+  modSala = geraResplendor(sala,x,y);
+  
 else
-    switch acao
-        case 1  %ação 1 -> acima
-            if x == 2  %controla se está na primeira linha
-                modX = x;
-                modY = y;
-            else
-                modX = x-1;
-                modY = y;
-            end
-        case 2  %ação 2 -> abaixo
-            if x == 5  %controla se está na última linha
-                modX = x;
-                modY = y;
-            else
-                modX = x+1;
-                modY = y;
-            end
-        case 3  %ação 2 -> esquerda
-            if y == 2  %controla se está na primeira coluna
-                modX = x;
-                modY = y;
-            else
-                modX = x;
-                modY = y-1;
-            end
-        case 4  %ação 2 -> direita
-            if y == 5  %controla se está na última coluna
-                modX = x;
-                modY = y;
-            else
-                modX = x;
-                modY = y+1;
-            end
-    end
+  switch acao
+    case 1 %ação 1 -> acima
+      if x == 2  %controla se está na primeira linha
+        modX = x;
+        modY = y;
+      else
+        modX = x-1;
+        modY = y;
+      end
+    case 2 %ação 2 -> abaixo
+      if x == 5  %controla se está na última linha
+        modX = x;
+        modY = y;
+      else
+        modX = x+1;
+        modY = y;
+      end
+    case 3 %ação 3 -> esquerda
+      if y == 2  %controla se está na primeira coluna
+        modX = x;
+        modY = y;
+      else
+        modX = x;
+        modY = y-1;
+      end
+    case 4 %ação 4 -> direita
+      if y == 5  %controla se está na última coluna
+        modX = x;
+        modY = y;
+      else
+        modX = x;
+        modY = y+1;
+      end
+    case 7 %ação 7 -> acima-esquerda
+      if sala(x-1,y-1) == parede %controla se não é parede o destino
+        modX = x;
+        modY = y;
+      else
+        modX = x-1;
+        modY = y-1;
+      end
+    case 8 %ação 8 -> acima-direita
+      if sala(x-1,y+1) == parede %controla se não é parede o destino
+        modX = x;
+        modY = y;
+      else
+        modX = x-1;
+        modY = y+1;
+      end
+    case 9 %ação 9 -> abaixo-esquerda
+      if sala(x+1,y-1) == parede %controla se não é parede o destino
+        modX = x;
+        modY = y;
+      else
+        modX = x+1;
+        modY = y-1;
+      end
+    case 10 %ação 10 -> abaixo-direita
+      if sala(x+1,y+1) == parede %controla se não é parede o destino
+        modX = x;
+        modY = y;
+      else
+        modX = x+1;
+        modY = y+1;
+      end
+  end
 end
 end
